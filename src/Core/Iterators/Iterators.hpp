46bee6c8f986a070bf09b54ddcde32eef246e44f
Quentin LADEVIE <qladevie@wanadev.fr>
2018-12-19 20:40:19 +0100

    Rework project architecture WIP

--- src/Core/Iterators/Iterators.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef __ITERATORS_HPP_
-#define __ITERATORS_HPP_
-
-#include "Iterator.hpp"
-#include "LinearStackIterator.hpp"
-
-#endif //__ITERATORS_HPP_
--- src/Core/Platform/Memory.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef __PLATFORM_MEMORY_HPP_
-#define __PLATFORM_MEMORY_HPP_
-
-
-#if defined (WINDOWS)
-	#include "Windows/WindowsMemory.hpp"
-#endif
-
-#endif //__PLATFORM_MEMORY_HPP_
