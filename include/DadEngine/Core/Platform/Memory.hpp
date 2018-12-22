9f80fb721e3457b88fcd59ee4041a5a47ec33200
Quentin <ladevie.quentin@gmail.com>
2018-12-22 12:17:56 +0100

    Remove directory with incorrect names

--- include/DadEngine/Core/Platform/Memory.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef __PLATFORM_MEMORY_HPP_
-#define __PLATFORM_MEMORY_HPP_
-
-
-#if defined (WINDOWS)
-	#include "windows/windows-memory.hpp"
-#endif
-
-#endif //__PLATFORM_MEMORY_HPP_
--- include/DadEngine/Rendering/Features.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef __FEATURES_HPP_
-#define __FEATURES_HPP_
-
-#include "feature/render-component.hpp"
-#include "feature/static-mesh-component.hpp"
-#include "feature/rendering-feature.hpp"
-#include "feature/static-mesh-rendering-feature.hpp"
-
-#endif //__FEATURES_HPP_
